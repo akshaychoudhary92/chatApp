ef2279c495c6ed02ec4d53a0b9d6182dd68ed528
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2018-02-11 21:37:42 -0500

    Add basic socket.io connections
    Hooked up express to <projectRoot>/javascript
    Added script tags to HTML
    Added base socket to <projectRoot>/app.js

--- app.js
+++ app.js
@@ -2,15 +2,30 @@ const http = require('http');
 const path = require('path');
 
 const express = require('express');
+const socketIO = require('socket.io');
 
+// region EXPRESS
 const app = express();
 const server = http.createServer(app);
 
 app.set('port', process.env.PORT || 8080);
 const port = app.get('port');
 
+app.use('/js', express.static(path.join(__dirname, 'javascript')));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+// endregion EXPRESS
+
+// region SOCKET.io
+
+const io = socketIO(server);
+
+io.on('connection', socket => {
+    console.log('A user connected');
+});
+
+// endregion SOCKET.io
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
