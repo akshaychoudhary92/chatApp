85c536ede553b02069989001de1b3a1f522bc84f
Christopher Aguilar <christopher.m.aguilar@gmail.com>
2018-02-12 14:22:47 -0500

    Change react-hot-loader to require instead of import
    Half the time it works, half the time it doesn't, I'm confused x-x

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { hot } from 'react-hot-loader';
 import * as io from 'socket.io-client';
 
 import * as styles from '~/client/styles/App.scss';
+
+const { hot } = require('react-hot-loader');
 
 const socket = io();
 
